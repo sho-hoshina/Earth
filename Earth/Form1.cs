89ca28f62bb4b5ae021efac6733c43b149d396a7
sho hoshina <hoshina-sho@jfe-eng.co.jp>
2024-10-23 11:22:53 +0900

    test message

--- Earth/Form1.cs
+++ Earth/Form1.cs
@@ -6,5 +6,10 @@ namespace Earth
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("test");
+        }
     }
 }
